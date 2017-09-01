ab71d37aa2cbe434e9253e289971d359da0b32c1
pieterhouwen <pieter_houwen@hotmail.com>
2017-09-01 19:53:28 +0200

    now reads config files

--- gitfinder.sh
+++ gitfinder.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo Searching for git repositories...
-cd / && find -name ".git" -type d 2>/dev/null >~/gitfinder/gitlist
-cd / && find -name ".git" -type d 2>/dev/null | sed "s/\/.git//" >~/gitfinder/dirlist
+cd / ; find -name ".git" -type d 2>/dev/null >~/gitfinder/gitlist
+cd / ; find -name ".git" -type d 2>/dev/null | sed "s/\/.git//" >~/gitfinder/dirlist
 
-for url in $(cat ~/gitfinder/gitlist 2>/dev/null)
+
+for url in $(cat gitlist)
 do
-cd $url && cat config 2>/dev/null
-continue
+cd $url ; cat config 2>/dev/null
 done
 
 
