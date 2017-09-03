de7cf85cfff29d566c6755a302d02ba6dd7a98dd
Pieter Houwen <pieter_houwen@hotmail.com>
2017-09-03 14:31:26 -0400

    Now has local locations correct

--- gitfinder.sh
+++ gitfinder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo Searching for git repositories...
 cd / ; find -name ".git" -type d 2>/dev/null >~/gitfinder/gitlist
-cd / ; find -name ".git" -type d 2>/dev/null | sed "s/\/.git//" >~/gitfinder/dirlist
+
 
 for url in $(cat ~/gitfinder/gitlist | grep -v dradis | cut -c2-)
 do
-cd $url ;  cat config 2>/dev/null | grep "http" >>~/gitfinder/completedlist.txt ; pwd >>~/gitfinder/completedlist.txt 
+cd $url ;  cat config 2>/dev/null | grep "http" | cut -c8- >>~/gitfinder/completedlist.txt ; cd .. && pwd >>~/gitfinder/completedlist.txt 
 done
 echo Results have been saved to completedlist.txt
 
