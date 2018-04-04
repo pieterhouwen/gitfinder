fcdc7ced0c8c082e10045d65200a375a27e87c26
root <root@localhost.localdomain>
2018-04-04 21:07:59 +0200

    Added support requirements.txt

--- gitdownloader.sh
+++ gitdownloader.sh
@@ -1,4 +1,11 @@
+curdir=$(pwd)
 paste urllist dirlist | while read url dir
 do
 git clone $url $dir
+done
+echo Done cloning!
+echo Installing requirements. . .
+for i in $(cat dirlist)
+do
+cd $i && pip install -r requirements.txt
 done
