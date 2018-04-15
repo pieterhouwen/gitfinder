f59e0266525374d919c59e6836c2e2d8e85bda48
root <root@localhost.localdomain>
2018-04-15 15:35:35 +0200

    Meer beter

--- gitdownloader.sh
+++ gitdownloader.sh
@@ -1,11 +1,42 @@
 curdir=$(pwd)
-paste urllist dirlist | while read url dir
-do
-git clone $url $dir
-done
+#paste urllist dirlist | while read url dir
+#do
+#git clone $url $dir
+#done
+
+#removes old logs
+rm piplog
+rm makelog
+
 echo Done cloning!
-echo Installing requirements. . .
-for i in $(cat dirlist)
+echo Installing requirements. . . This will take a while, go grab a cup of covfefe.
+echo
+
+# This function  Search Location for $1 wich is file type then store results in $2
+function search_for() {
+echo 
+echo "create $1 list store in $2" #debug output
+	(
+		cd / ; find -name $1 -type f 2>/dev/null | grep -v dradis | grep -v autopwn | cut -c2- >$curdir/$2
+	)
+}
+
+# trigger functions
+search_for requirements.txt reqlist
+search_for Makefile makelist
+
+for require in $(cat $curdir/reqlist)
 do
-cd $i && pip install -r requirements.txt
+pip install -r $require --log $curdir/piplog 1>/dev/null
 done
+echo Done installing requirements!
+echo Making some magic with magic makefiles
+for makef in $(cat $curdir/makelist)
+do
+make $makef clean 2>>$curdir/makelog ; make $makef 2>>$curdir/makelog ; make $makef install 2>>$curdir/makelog
+done
+
+#function searchfiles {
+#for file in $(cat dirlist)
+#do 
+#cd $file && find -name $1 
