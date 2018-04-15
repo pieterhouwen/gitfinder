5f2c3d39ab7dd34d0b17272c03737115d0bd5752
root <root@localhost.localdomain>
2018-04-15 15:42:13 +0200

    Added original functionality

--- gitdownloader.sh
+++ gitdownloader.sh
@@ -1,20 +1,20 @@
 curdir=$(pwd)
-#paste urllist dirlist | while read url dir
-#do
-#git clone $url $dir
-#done
-
+echo Begin git cloning. . .
+paste urllist dirlist | while read url dir
+do
+git clone $url $dir
+done
+echo Done cloning!
 #removes old logs
+echo Cleaning up old log files.
 rm piplog
 rm makelog
 
-echo Done cloning!
 echo Installing requirements. . . This will take a while, go grab a cup of covfefe.
 echo
 
 # This function  Search Location for $1 wich is file type then store results in $2
 function search_for() {
-echo 
 echo "create $1 list store in $2" #debug output
 	(
 		cd / ; find -name $1 -type f 2>/dev/null | grep -v dradis | grep -v autopwn | cut -c2- >$curdir/$2
@@ -27,7 +27,7 @@ search_for Makefile makelist
 
 for require in $(cat $curdir/reqlist)
 do
-pip install -r $require --log $curdir/piplog 1>/dev/null
+pip install -r $require --log $curdir/piplog 1>/dev/null 2>>$curdir/piplog
 done
 echo Done installing requirements!
 echo Making some magic with magic makefiles
